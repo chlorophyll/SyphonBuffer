81d74347ab5d5ed66c3a004d7b75b4d13a585ef2
Chris Williamson <c.williamso@gmail.com>
2019-06-25 23:00:20 -0700

    Don't attempt to build on systems without OSX frameworks

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+SYSTEM=$(uname -s)
+if [[ "$SYSTEM" != "Darwin" ]]; then
+    echo "SyphonBuffer: Platform ($SYSTEM) not supported."
+    echo "SyphonBuffer: Skipping build."
+    exit 1;
+fi
+
 cd native
 make
